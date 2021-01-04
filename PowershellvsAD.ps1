4bc3cceaba8ecf651ac649f2e6c81b2bd9d1358e
KH-GitGud <74725751+KH-GitGud@users.noreply.github.com>
2021-01-04 16:49:21 -0700

    Update PowershellvsAD.ps1

--- PowershellvsAD.ps1
+++ PowershellvsAD.ps1
@@ -1,14 +1,21 @@
 # Script intended to prep devices for reimaging by removing their entries in AD and SCCM, in that order - AD is easier to search.
-# Need to import module first: https://docs.microsoft.com/en-us/powershell/sccm/overview?view=sccm-ps
-# Run from SCCM console?
+
+#Import AD first
+Import-Module ActiveDirectory
 
 # Ask for PCC Number, ideally pipe to AD to retrieve full computer name and use that as our variable
-$PCC = Read-Host "Please enter the PCC Number of the computer to be removed" | Get-ADComputer -Filter # Ask Justin?
+$PCC = Read-Host "Enter a PCC Number:" | Get-ADComputer -Filter {Name -like $PCC}
 
-# How to enter large amounts of PCC numbers at once? Manual type? CSV?
+# How to enter large amounts of PCC numbers at once? Manual type? CSV? JSON array?
 
 # AD Reference: https://docs.microsoft.com/en-us/powershell/module/addsadministration/?view=win10-ps
 Remove-ADComputer -Identity -Confirm -WhatIf $PCC
+
+# Imports SCCM to PowerShell: https://docs.microsoft.com/en-us/powershell/sccm/overview?view=sccm-ps
+Set-Location 'C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin'
+Import-Module .\ConfigurationManager.psd1
+(Get-Module -Name ConfigurationManager).Version
+(Get-Module -Name ConfigurationManager).Path
 
 # SCCM Reference: https://docs.microsoft.com/en-us/powershell/module/configurationmanager/remove-cmdevice?view=sccm-ps
 Get-CMDevice -Name $PCC | Remove-CMDevice -Confirm -WhatIf
