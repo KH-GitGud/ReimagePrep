b83e6ff8c577f73092ca98e5e2f98658be9c6de9
KH-GitGud <74725751+KH-GitGud@users.noreply.github.com>
2021-01-06 14:07:40 -0700

    Update PowershellvsAD.ps1

--- PowershellvsAD.ps1
+++ PowershellvsAD.ps1
@@ -4,14 +4,16 @@
 Import-Module ActiveDirectory
 
 # Ask for PCC Number, ideally pipe to AD to retrieve full computer name and use that as our variable
-$PCC = Read-Host "Enter a PCC Number:" | Get-ADComputer -Filter { Name -like $PCC } #Currently returns "$PCC is not defined"
-# Use Get-Wmi-Object instead?
-# BTW -match '^\d{6}$' is RegEx for PCC Number. Thanks Will!
+$PCC = Read-Host 'Enter a PCC Number'
+Get-ADComputer -Filter ('Name -Like "*' + $PCC + '*"')  -Server PCC-Domain.pima.edu # Thanks to Will and Justin for proofreading format
+Get-ADComputer -Filter ('Name -Like "*' + $PCC + '*"')  -Server EDU-Domain.pima.edu # BTW -match '^\d{6}$' is RegEx for PCC Number if necessary. Thanks Will!
+
+
 
 # How to enter large amounts of PCC numbers at once? Manual type? CSV? JSON array?
 
 # AD Reference: https://docs.microsoft.com/en-us/powershell/module/addsadministration/?view=win10-ps
-Remove-ADComputer -Identity -Confirm -WhatIf $PCC
+#Remove-ADComputer -Identity -Confirm -WhatIf $PCC
 
 # Imports SCCM to PowerShell: https://docs.microsoft.com/en-us/powershell/sccm/overview?view=sccm-ps
 Set-Location 'C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin'
@@ -20,5 +22,5 @@ Import-Module .\ConfigurationManager.psd1
 (Get-Module -Name ConfigurationManager).Path
 
 # SCCM Reference: https://docs.microsoft.com/en-us/powershell/module/configurationmanager/remove-cmdevice?view=sccm-ps
-Get-CMDevice -Name $PCC | Remove-CMDevice -Confirm -WhatIf
+#Get-CMDevice -Name $PCC | Remove-CMDevice -Confirm -WhatIf
 # I think this should work, given SCCM does not require a full PC name to search
