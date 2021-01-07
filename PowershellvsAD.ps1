a8f4a588414e7ab20fece39e689d4ee0ced56a02
KH-GitGud <74725751+KH-GitGud@users.noreply.github.com>
2021-01-06 17:00:19 -0700

    Update PowershellvsAD.ps1

--- PowershellvsAD.ps1
+++ PowershellvsAD.ps1
@@ -1,26 +1,21 @@
-# Script intended to prep devices for reimaging by removing their entries in AD and SCCM, in that order - AD is easier to search.
+# Script intended to prep devices for reimaging by removing their entries in AD and SCCM, in that order.
+# Thanks to Will and Justin for proofreading format
+# BTW -match '^\d{6}$' is RegEx for PCC Number if necessary. Thanks Will!
 
-#Import AD first
-Import-Module ActiveDirectory
-
-# Ask for PCC Number, ideally pipe to AD to retrieve full computer name and use that as our variable
-$PCC = Read-Host 'Enter a PCC Number'
-Get-ADComputer -Filter ('Name -Like "*' + $PCC + '*"')  -Server PCC-Domain.pima.edu # Thanks to Will and Justin for proofreading format
-Get-ADComputer -Filter ('Name -Like "*' + $PCC + '*"')  -Server EDU-Domain.pima.edu # BTW -match '^\d{6}$' is RegEx for PCC Number if necessary. Thanks Will!
-
-
-
-# How to enter large amounts of PCC numbers at once? Manual type? CSV? JSON array?
-
-# AD Reference: https://docs.microsoft.com/en-us/powershell/module/addsadministration/?view=win10-ps
-#Remove-ADComputer -Identity -Confirm -WhatIf $PCC
-
-# Imports SCCM to PowerShell: https://docs.microsoft.com/en-us/powershell/sccm/overview?view=sccm-ps
+# Imports first
+# https://docs.microsoft.com/en-us/powershell/sccm/overview?view=sccm-ps
 Set-Location 'C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin'
 Import-Module .\ConfigurationManager.psd1
-(Get-Module -Name ConfigurationManager).Version
-(Get-Module -Name ConfigurationManager).Path
+    (Get-Module -Name ConfigurationManager).Version
+    (Get-Module -Name ConfigurationManager).Path
+Import-Module ActiveDirectory
+
+# Retrieves full name from AD using PCC number, then removes
+# AD Reference: https://docs.microsoft.com/en-us/powershell/module/addsadministration/?view=win10-ps
+$PCC = Read-Host 'Enter a PCC Number'
+$Domain = Read-Host 'Enter Domain (PCC or EDU)'
+Get-ADComputer -Filter ('Name -Like "*' + $PCC + '*"')  -Server $Domain-Domain.pima.edu | Remove-ADComputer -Confirm -WhatIf
 
 # SCCM Reference: https://docs.microsoft.com/en-us/powershell/module/configurationmanager/remove-cmdevice?view=sccm-ps
-#Get-CMDevice -Name $PCC | Remove-CMDevice -Confirm -WhatIf
+Get-CMDevice -Name $PCC | Remove-CMDevice -Confirm -WhatIf
 # I think this should work, given SCCM does not require a full PC name to search
