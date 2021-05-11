54774f7b2aff4b0cd0d754822918fd19dbdd18dc
KH-GitGud <74725751+KH-GitGud@users.noreply.github.com>
2021-05-11 12:36:33 -0700

    What switches do...
    Code still functions for CSVs generated by a CS3070. Future updates will generalize the CSV option and move the actual device removal outside of the switch statement.

--- PowershellvsAD.ps1
+++ PowershellvsAD.ps1
@@ -31,29 +31,32 @@ catch {
 }
 
 # Needed here: ask user if barcode data is coming from a csv file or direct input (scanner/keyboard)
-# Trying a Switch function. Prompts user, now where to use $UserSelect...
+# Currently runs with CSVs SPECIFICALLY from a Motorola Symbol CS3070 scanner, working on Direct Scan
+# Later: generalize the CSV input. Other things aside from this specific scanner use them.
 $InputType = Read-Host "Are you loading from a file (CSV) or scanning PCC numbers directly (Scan)?"
-    switch ($InputType) {
-        CSV { $UserSelect = "CSV of Scanned Barcodes" }
-        Scan { $UserSelect = "Direct Scan" }
-        default { "Input not recognized. Please enter either 'CSV' or 'Scan' to proceed." }
+switch ($InputType) {
+    CSV {
+        # ORIGINAL, FUNCTIONING CODE STARTS HERE... 
+        # Retrieves a list of numbers in the rightmost column
+        $DriveLetter = (Get-Volume -Friendlyname CS3070).DriveLetter + { :\ }
+        $NumbersCol = (Import-CSV $DriveLetter'Scanned Barcodes\BARCODES.txt' -Header 'DateScanned', 'TimeScanned', 'Unknown', 'Barcode').Barcode
+        $PCCRegEx = '^\d{6}$'
+            
+        # Ensure numbers are PCC numbers, then search AD and remove them
+        # Does this work without the $NumberCol variable? If so, move this chunk to after the switch, the same process must happen regardless of $InputType
+        Import-Module ActiveDirectory
+        foreach ($Barcode in $NumbersCol) {
+            if ($Barcode -match $PCCRegEx) {
+                $WildBars = 'Name -Like "*' + $Barcode + '*"'
+                (Get-CMDevice -Name *$Barcode*).name
+                Get-ADComputer -Filter ($WildBars) -Server PCC-Domain.pima.edu | Remove-ADComputer -Confirm
+                Get-ADComputer -Filter ($WildBars) -Server EDU-Domain.pima.edu | Remove-ADComputer -Confirm
+            }
+        } 
     }
-
-
-# ORIGINAL, FUNCTIONING CODE STARTS HERE... SPECIFICALLY for a Motorola Symbol CS3070 scanner
-# Will be reorganized shortly to run if $InputType = CSV
-# Retrieves a list of numbers in the rightmost column
-$DriveLetter = (Get-Volume -Friendlyname CS3070).DriveLetter + { :\ }
-$NumbersCol = (Import-CSV $DriveLetter'Scanned Barcodes\BARCODES.txt' -Header 'DateScanned', 'TimeScanned', 'Unknown', 'Barcode').Barcode
-$PCCRegEx = '^\d{6}$'
-
-# Ensure numbers are PCC numbers, then search AD and remove them
-Import-Module ActiveDirectory
-foreach ($Barcode in $NumbersCol) {
-    if ($Barcode -match $PCCRegEx) {
-        $WildBars = 'Name -Like "*' + $Barcode + '*"'
-        (Get-CMDevice -Name *$Barcode*).name
-        Get-ADComputer -Filter ($WildBars) -Server PCC-Domain.pima.edu | Remove-ADComputer -Confirm
-        Get-ADComputer -Filter ($WildBars) -Server EDU-Domain.pima.edu | Remove-ADComputer -Confirm
-    }
+    Scan { $Barcode = Read-Host }
+    default { "Input not recognized. Please enter either 'CSV' or 'Scan' to proceed." }
 }
+
+
+
